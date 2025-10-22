0ed4ec4ffd437954c36e8bd766a05d5a299e5e5c
samrithireddybo1 <152311022+samrithireddybo1@users.noreply.github.com>
2025-10-22 10:39:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('myform.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -23,7 +23,7 @@ def submit():
 @app.route('/result')
 def result():
     username = request.args.get('username')
-    return render_template('greeting.html', username=username)
+    return render_template('result.html', username=username)
 
 if __name__ == '__main__':
     app.run(debug=True)
